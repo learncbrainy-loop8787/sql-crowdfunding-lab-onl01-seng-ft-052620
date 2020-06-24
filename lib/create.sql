fb5cf06253c3da88e4321981d055c4a4d9d1f8db
korey ko <koreykendrick1@gmail.com>
2020-06-24 15:51:38 +0000

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,37 @@
+CREATE TABLE projects
+(
+id INTEGER PRIMARY KEY,
+title TEXT,
+category TEXT,
+funding_goal FLOAT,
+start-date DATE,
+end_date DATE
+); 
+
+CREATE TABLE users 
+(
+id INTEGER PRIMARY KEY.
+name taxt,
+age INTEGER,
+);
+CREATE TABLE pledges
+( 
+id INTEGER PRIMARY KEY 
+amount FLOAT,
+user_id INTEGER,
+project_id INTEGER
+):
+
+
+
+
+
+
+
+
+
+
+
+
+
+)
