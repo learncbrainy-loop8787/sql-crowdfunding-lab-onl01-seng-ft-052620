10483e95565c8dbb97939ad84e3b2c086456a826
korey ko <koreykendrick1@gmail.com>
2020-06-25 03:47:44 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,7 +8,8 @@
 
 
 
-def selects_the_titles_of_all_projects_and_their_pledge_amounts
+def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
+
   "SELECT projects.title, SUM(pledges.amount) 
   FROM projects JOIN pledges ON projects.id = pledges.project_id 
   GROUP BY projects.title;"
