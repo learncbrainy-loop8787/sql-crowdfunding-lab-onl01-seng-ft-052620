71707ce4adb3b6671a817216c7d2296333a38673
korey ko <koreykendrick1@gmail.com>
2020-06-25 03:49:53 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
   "SELECT users.name, users.age, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name;"
 end
 
-def selects_the_titles_of_all_projects_that_have_met_their_funding_goal
+def 
     "SELECT projects.title, (projects.funding_goal -
     SUM(pledges.amount)) AS amount_left FROM projects 
     JOIN pledges ON projects.id = pledges.project_id GROUP BY projects.title HAVING amount_left <= 0;"
